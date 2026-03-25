5ab6947968782e8acfab83aacf359abcde4fbe70
Relaxed-Chowder <223363285+Relaxed-Chowder@users.noreply.github.com>
2026-03-25 14:05:54 -0700

    tilemaplayer script added

---
+++ octo-blocs/scripts/tile_map_layer.gd
@@ -0,0 +1,84 @@
+extends TileMapLayer
+
+#tetrominoes
+var i_0 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)]
+var i_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(1, 3)]
+var i_180 := [Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2), Vector2i(3, 2)]
+var i_270 := [Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2), Vector2i(2, 3)]
+var i := [i_0, i_90, i_180, i_270]
+
+var t_0 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var t_90 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
+var t_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var t_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var t := [t_0, t_90, t_180, t_270]
+
+var o_0 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var o := [o_0]
+
+var z_0 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)]
+var z_90 := [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]
+var z_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
+var z_270 := [Vector2i(2, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)]
+var z := [z_0, z_90, z_180, z_270]
+
+var s_0 := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1)]
+var s_90 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]
+var s_180 := [Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2), Vector2i(1, 2)]
+var s_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
+var s := [s_0, s_90, s_180, s_270]
+
+var l_0 := [Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var l_90 := [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2)]
+var l_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2)]
+var l_270 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)]
+var l := [l_0, l_90, l_180, l_270]
+
+var j_0 := [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)]
+var j_90 := [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 2), Vector2i(1, 2)]
+var j_180 := [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)]
+var j_270 := [Vector2i(1, 0), Vector2i(2, 0), Vector2i(1, 1), Vector2i(1, 2)]
+var j := [j_0, j_90, j_180, j_270]
+
+var tetrominoes := [i, t, o, z, s, l, j]
+var tetrominoes_full := tetrominoes.duplicate()
+
+#grid variables
+const COLS : int = 10
+const ROWS : int = 18
+
+#game piece variables
+var piece_type
+var next_piece_type
+var rotation_index : int = 0
+var active_piece : Array
+
+#tilemap variables
+var tile_id : int = 0
+var piece_atlas : Vector2i
+var next_piece_atlas : Vector2i
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func new_game():
+	tetrominoes.shuffle()
+	piece_type = pick_piece()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	draw_piece(j[0], Vector2i(5,1), Vector2i(1,0))
+	
+func pick_piece():
+	var piece
+	if not tetrominoes.is_empty():
+		piece = tetrominoes.pop_front()
+	
+	else:
+		tetrominoes = o[0].push_front()
+
+func draw_piece(piece, pos, atlas):
+	pass
+	for i in piece:
+		set_cell(pos+i, tile_id, atlas)
